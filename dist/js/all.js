e7fe6ad5a47295290ca9d462828400e800b2effe
AntonEssential <antonessential@hotmail.co.uk>
2016-05-14 18:11:59 +0300

    version 1.0.0

---
+++ dist/js/all.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  // js slider
+  $('.slider').bxSlider({
+    pager: false,
+    controls: false,
+    auto: true,
+    speed: 1000,
+    pause: 7000,
+    mode: 'fade'
+  });
+});
